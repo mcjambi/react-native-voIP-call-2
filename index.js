03002312b49b39e3d606a3dc0813160d1b6122fa
Hien Nguyen <mcjambi@gmail.com>
2022-08-03 16:26:19 +0700

    update
    update

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import { NativeModules, DeviceEventEmitter, Platform } from 'react-native';
+
+export RNVoipPushKitNativeModule from './lib/iosPushKit';
+export RNVoipCallNativeModule from './lib/RNVoipCall';
